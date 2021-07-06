7a6d968b00eb545a8af1d2c405efee44865abeb0
aa2082 <aa2082@cam.ac.uk>
2021-07-06 21:16:03 +0100

    shell script working

--- run_sim/run_multiple.sh
+++ run_sim/run_multiple.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-
-echo "the date is $(date)"
 echo $HOME
 
 mkdir "$HOME/$1/data"	#directory to hold /Cells1 /Cells2 ...
+mkdir "$HOME/$1/processing" #directory to hold current stuff
 mkdir "$HOME/$1/cmd_line_output" #directory to hold command line outputs: output1.txt output2.txt
 
 for ((i=1; i<= $2; i++))
 do
-	touch "$HOME/$1/cmd_line_output/output$i.txt"	#makes output file for recording command line of simulation
-	./$3 in1.txt $4 $HOME/$1/data > $HOME/$1/cmd_line_output/output$i.txt
+	echo "starting number $(i) at $(date)"
+	touch $HOME/$1/cmd_line_output/output$i.txt	#makes output file for recording command line of simulation
+	./$3 in1.txt $4 $HOME/$1/processing > $HOME/$1/cmd_line_output/output$i.txt
+	mkdir $HOME/$1/data/Cells$i
+	cp -R $HOME/$1/processing/Cells/* $HOME/$1/data/Cells$i
+	rm -r $HOME/$1/processing/*
 done
